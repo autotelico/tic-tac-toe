e5a3670d80d718c6351fd6746536cb3261e6a0a4
Henrique Heron <henriqueheron1@gmail.com>
2023-12-13 18:40:54 -0300

    create factory functions for startGame, createPlayer, and Gameboard

--- script.js
+++ script.js
@@ -1,31 +1,73 @@
-document.addEventListener("DOMContentLoaded", () => {
-    
-    const player1Name = document.querySelector('#player1-name');
-    const player1MarkerX = document.querySelector('input[value="X"');
-    const player1MarkerO = document.querySelector('input[value="O"');
-    const player2Name = document.querySelector('#player2-name');
-    const player2MarkerX = document.querySelector('input[value="X"]');
-    const player2MarkerO = document.querySelector('input[value="O"]');
+// document.addEventListener("DOMContentLoaded", () => {
 
-    const board = document.querySelector('#gameboard');
-    const cells = document.querySelectorAll('.cell');
+const Gameboard = (function() {
+    const board = [];
+    const rows = 3;
+    const columns = 3;
 
-    function createGame() {
-        const gameboard = [];
-        const startGameMessage = function() {
-            
-        }    
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(0);
+        }
     }
 
-    function createPlayer(name, marker) {
-        const playerName = name;
-        const playerMarker = marker;
-
-        return {playerName, playerMarker}
+    const getBoard = function () {
+        return board;
     }
 
-    const player1 = createPlayer(player1Name, player1Marker);
-    const player2 = createPlayer(player2Name, player2Marker);
+    return { getBoard }
+})();
 
 
-})
+// console.log(newBoard.getBoard());
+
+
+let selectedRow = 2;
+let selectedColumn = 1;
+
+// newBoard.getBoard()[selectedRow][selectedColumn] = 'X'; // will be player1Marker
+
+// console.log(newBoard.getBoard())
+
+
+function createPlayer(name, marker) {
+
+    const playerName = name;
+    const playerMarker = marker;
+
+    const getName = () => name;
+    const getMarker = () => marker;
+
+
+
+    return { getName, getMarker }
+}
+
+const player1 = createPlayer('Carl', 'X');
+const player2 = createPlayer('Mark', 'O');
+
+// console.log(player2.getName());
+
+
+const startGame = (function () {
+    const board = Gameboard.getBoard();
+
+    const getBoard = () => board;
+
+    const { playerName } = createPlayer();
+
+    const players = [];
+
+    players.push({ player1, player2 });
+
+    const getPlayers = () => players;
+    return { players, getPlayers, playerName, getBoard }
+})();
+
+
+
+console.log(startGame.getPlayers());
+console.log(startGame.getBoard());
+
+// })
