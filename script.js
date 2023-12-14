5274df1ab6ac716e9d29f37770506816bc9c5f3f
Henrique Heron <henriqueheron1@gmail.com>
2023-12-14 19:35:40 -0300

    add game display and make player turn switch

--- script.js
+++ script.js
@@ -1,34 +1,65 @@
-// document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
+
+    const gameboard = document.querySelector('#gameboard');
+
+    const cell1 = document.querySelector('#cell-1');
+    const cell2 = document.querySelector('#cell-2');
+    const cell3 = document.querySelector('#cell-3');
+    const cell4 = document.querySelector('#cell-4');
+    const cell5 = document.querySelector('#cell-5');
+    const cell6 = document.querySelector('#cell-6');
+    const cell7 = document.querySelector('#cell-7');
+    const cell8 = document.querySelector('#cell-8');
+    const cell9 = document.querySelector('#cell-9');
+
+
+    gameboard.addEventListener('click', (e) => {
+        const chosenCell = e.target;
+
+        if (chosenCell === gameboard) {
+            e.preventDefault();
+        }
+
+        chosenCell.textContent = GameController.getActivePlayer().getMarker();
+        console.log(chosenCell.textContent);
+
+        GameController.switchActivePlayer();
+
+    })
 
 const Gameboard = (function () {
     const board = [];
     const rows = 3;
     const columns = 3;
 
+    
     for (let i = 0; i < rows; i++) {
         board[i] = [];
         for (let j = 0; j < columns; j++) {
             board[i].push(0);
         }
     }
-
+    
     const getBoard = function () {
         return board;
     }
+    
+    const createCell = () => {
+        const div = document.createElement('div');
+        div.classList.add('cell');
+        gameboard.appendChild(div);
+    }
 
+    const displayBoard = () => {
+        for (let i = 0; i < rows * columns; i++) {
+            createCell();
+        }
+    }
 
-    return { getBoard }
+    return { getBoard, displayBoard }
 })();
 
-
-
-
-let selectedRow = 2;
-let selectedColumn = 1;
-
-// newBoard.getBoard()[selectedRow][selectedColumn] = 'X'; // will be player1Marker
-
-
+Gameboard.displayBoard();
 
 function createPlayer(name, marker) {
 
@@ -43,26 +74,28 @@ function createPlayer(name, marker) {
     return { getName, getMarker, getScore }
 }
 
-const player1 = createPlayer('Player 1', 'X');
-const player2 = createPlayer('Player 2', 'O');
+const player1 = createPlayer('Player 1', '✖');
+const player2 = createPlayer('Player 2', '◯');
 
 
 const GameController = (function () {
 
     const players = [];
-    players.push({ player1 }, { player2 });
+    players.push(player1, player2);
+    console.log('Players: ' + players);
 
     const board = Gameboard.getBoard();
 
     const getBoard = () => board;
     const getPlayers = () => players;
 
-    const activePlayer = players[0];
+    let activePlayer = players[0];
   
     const getActivePlayer = () => activePlayer;
   
     const switchActivePlayer = () => {
       activePlayer === players[0] ? activePlayer = players[1] : activePlayer = players[0];
+      console.log(`Current player is ${activePlayer.getName()}.`);
     }
   
     return { getPlayers, getBoard, getActivePlayer, switchActivePlayer }
@@ -97,8 +130,7 @@ function evaluateWin() {
     }))
 }
 
-console.log('Active player is: ' + GameController.getActivePlayer().getName());
 
 
 
-// })
+})
